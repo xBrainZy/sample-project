b2edec63d74cef410b6c48b67d52df0b1621e535
xBrainZy <cocoacola00729@gmail.com>
2025-02-13 13:39:48 +0300

    Added About us component

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section>
+      <h2>About Us</h2>
+      <p>We are a leading camera company dedicated to providing the best photography equipment for professionals and enthusiasts alike.</p>
+    </section>
+  );
+};
+
+export default About;
