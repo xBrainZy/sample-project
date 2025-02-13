11a2e7ed62b78566d67c95018cfc78ecadc6ba9f
xBrainZy <cocoacola00729@gmail.com>
2025-02-13 12:07:48 +0300

    Created a hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <section>
+      <h1>Capture the World with Camera Co.</h1>
+      <p>High-quality cameras for every moment.</p>
+      <button>Shop Now</button>
+    </section>
+  );
+};
+
+export default Hero;
